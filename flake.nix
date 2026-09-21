5f6c75d65dc995797af40bd6b0042d201d092127
CocoIsBuggy <g.colinharley@gmail.com>
2026-09-21 09:34:01 +0200

    chore: Add a nix flake definition for this dev env
    This doesn't build the application, it's just a little dev env for my
    nixos users out there.

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        nodejs = pkgs.nodejs_22;
+        yarn = pkgs.yarn.override { inherit nodejs; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "sandbag-dev";
+          packages = [
+            nodejs
+            yarn
+            pkgs.typescript
+          ];
+        };
+      }
+    );
+}
